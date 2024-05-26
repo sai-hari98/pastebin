c49b74a48fddd2a356b8f5162a2fb4246139de51
Sairam Harikrishnan <sai_hari98@hotmail.com>
2024-05-26 18:52:25 -0500

    added date and time picker to set expiry for the text

--- pastebin-ui/src/components/editor/Editor.js
+++ pastebin-ui/src/components/editor/Editor.js
@@ -1,15 +1,19 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import pastebinAxios from '../../pastebin-axios';
+import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
+import './Editor.css'
 
 class Editor extends Component {
 
     state = {
-        text : null,
-        textValid : false
+        text: null,
+        textValid: false
     }
 
-    saveText(){
-        pastebinAxios.post("/text",this.state.text, {headers:{'Content-Type':'text/plain'}}).then(response => {
+    saveText() {
+        pastebinAxios.post("/text", this.state.text, { headers: { 'Content-Type': 'text/plain' } }).then(response => {
             console.log(response);
         }).catch(error => {
             console.log(error);
@@ -18,19 +22,27 @@ class Editor extends Component {
 
     onChangeText(newText) {
         const isTextValid = newText !== null || newText !== undefined;
-        this.setState({text : newText, textValid : isTextValid});
+        this.setState({ text: newText, textValid: isTextValid });
     }
 
-    render(){
+    render() {
         return (
-        <div class="row mt-5">
-            <div class="col-12">
-                <textarea rows="20" cols="100" placeholder="Enter your text to be shared..." value={this.state.text} onChange={(event) => this.onChangeText(event.target.value)}></textarea>
-            </div>
-            <div className="col-12">
-                <button className="btn btn-primary" onClick={() => this.saveText()} disabled={!this.state.textValid}>Publish text</button>
-            </div>
-        </div>
+            <>
+                <h4 className="mt-5">Share new text with your friends...</h4>
+                <div className="row mt-2 p-3">
+                    <div className="col-4">
+                        <textarea className="textbox" rows="2" placeholder="Enter your text to be shared..." value={this.state.text} onChange={(event) => this.onChangeText(event.target.value)}></textarea>
+                    </div>
+                    <div className="col-4">
+                        <LocalizationProvider dateAdapter={AdapterMoment} adapterLocale='en'>
+                            <DateTimePicker placeholder="Set Expiry date and time" />
+                        </LocalizationProvider>
+                    </div>
+                    <div className="col-4 d-flex align-items-center">
+                        <button className="btn btn-primary" onClick={() => this.saveText()} disabled={!this.state.textValid}>Publish text</button>
+                    </div>
+                </div>
+            </>
         );
     }
 }
